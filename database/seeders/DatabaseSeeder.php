a70a3e246b04f4777126e4bdb29f542cd40b9219
vicheaa <sarosereyvichea@gmail.com>
2026-02-08 07:40:45 +0700

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             MovementTypeSeeder::class,
+            OrderSeeder::class,
         ]);
     }
 }
